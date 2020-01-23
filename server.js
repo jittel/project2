7f162548326fdc612e1106b9dbd77d82636714a3
Henry Lin <henrylin58@gmail.com>
2020-01-23 11:33:52 -0800

    added user controllers

--- server.js
+++ server.js
@@ -25,10 +25,13 @@ app.use(express.static("public"));
 // =============================================================
 // require("./controllers/controller.js")(app);
 
+const userRoutes = require('./controllers/userController');
+app.use(userRoutes);
+
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+db.sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
 });
