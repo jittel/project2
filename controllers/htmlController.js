6af940e31454aaf9cb8c25c036519bffb0c68606
Arauco <arishel@gmail.com>
2020-01-23 13:28:11 -0800

    created all html routes

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -6,8 +6,56 @@ var router = express.Router();
 var db = require("../models");
 
 
-router.get('/',function(req,res){
-    res.json('poop');
+router.get('/', function (req, res) {
+    db.task.find({})
+        .then(tasks => {
+
+            res.render("home", {
+                tasks
+            });
+        })
+})
+
+router.get('/task', function (req, res) {
+    db.task.find({})
+        .then(tasks => {
+
+            res.render("task", {
+                tasks
+            });
+        })
+})
+
+router.get('/user', function (req, res) {
+    db.task.find({})
+        .then(tasks => {
+
+            res.render("userpage", {
+                tasks
+            });
+        })
+})
+
+router.get('/login', function (req, res) {
+
+    res.render("login");
+
+})
+
+router.get('/create-acc', function (req, res) {
+
+    res.render("create-acc");
+
+})
+
+router.get('/add', function (req, res) {
+    db.task.find({})
+        .then(tasks => {
+
+            res.render("add", {
+                tasks
+            });
+        })
 })
 
 module.exports = router;
