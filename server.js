f5a1db5c3986a621dfc303de48cc377d319d4675
Henry Lin <henrylin58@gmail.com>
2020-01-24 15:34:21 -0800

    getting all tasks

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(userRoutes);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
     });
