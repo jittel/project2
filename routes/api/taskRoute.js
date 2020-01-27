29f36f83f0b0cba34469096fafc659ed7cb49979
jittel <nathan.c.ginn@gmail.com>
2020-01-26 18:10:10 -0800

    working on adding a task

--- routes/api/taskRoute.js
+++ routes/api/taskRoute.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const taskController = require("../../controllers/api/taskController");
 
 router.route("/").get(taskController.allTask);
+router.route("/new").post(taskController.newTask);
 router.route("/:id")
     .get(taskController.singleTask)
     .put(taskController.updateTask)
     .delete(taskController.deleteTask);
-router.route("/new").post(taskController.newTask);
 
 module.exports = router;
